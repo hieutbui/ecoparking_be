ac97ca53cd75803fcbc0bedce28748615988211d
hieubt <80142234+hieutbui@users.noreply.github.com>
2023-05-27 22:33:11 +0700

    config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-shadow': 'off',
+    'no-undef': 'off',
+    'react-native/no-inline-styles': 'off',
+    radix: 'off',
+    semi: [2, 'always'],
+    'prefer-const': [
+      'warn',
+      { ignoreReadBeforeAssign: false, destructuring: 'any' },
     ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    'no-duplicate-imports': ['error', { includeExports: false }],
+    '@typescript-eslint/consistent-type-imports': 'off',
+    'spaced-comment': 'off',
+    'prettier/prettier': 'off',
+  },
+};
