1c5042d5e2c3c824d6bfadf1fb76821ee4ed5f2f
MrDungRose <mr.trungdung113@gmail.com>
2023-07-11 21:55:48 +0700

    UPDATE: extendTime

--- src/repositories/ticketDetails.js
+++ src/repositories/ticketDetails.js
@@ -1,9 +1,32 @@
 import { OutputType, print } from '../helpers/print.js';
-
-const demo = async () => {
-  print('demo from ticket detail', OutputType.INFORMATION);
-};
+import Parking from '../models/Parking.js';
+const calculateExtendFee = async ( Duration, extendPrice,TotalPrice, extendDuration, parkingId) => {
+  try {
+    const parking = await Parking.findById(parkingId).populate('unitPrice');
+    const { price } = parking.unitPrice;
+    
+   let extendPrice = 0;
+  /*
+    if (chosenTime)
+    {
+      extendPrice = extendTime*price
+    } else (!chosenTime && extendTime) {
+      extendPrice = (checkoutHour-chosenTime)*price*0.15
+    }
+   */
+  
+  
+    if (Duration < 1) {
+      alert('Hãy mua thêm giờ');
+      extendPrice = extendDuration*price;
+    } else (!extendDuration && extendTime); {
+      TotalPrice = Duration + (extendDuration*price*0.15);
+    }
+  } catch (error) {
+    throw new Error (`Failed to calculate the extend parking fee. Error: ${err.message}`);
+  }
+}
 
 export default {
-  demo,
+  calculateExtendFee,
 };
